c3972c3c4d13439607989ce4fd49f3efd1e76ded
Akhila K C <akhilakc4747@gmail.com>
2025-06-25 19:55:13 +0530

    Update Axios baseURL to live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // FastAPI server URL
+  baseURL: "https://splitwise-zv50.onrender.com", // FastAPI server URL
 });
 
 export default api;
+
+// 'http://localhost:8000'
